230f392584b6d90f83c77c2259c88d5fec96d24d
Ashir <ashirq@outlook.com>
2020-04-15 17:03:59 +0100

    shows all journey ends

--- models/traveller.js
+++ models/traveller.js
@@ -10,7 +10,10 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const journeyEnds = this.journeys.map ( (journey) => {
+    return journey.endLocation;
+  })
+  return journeyEnds;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
